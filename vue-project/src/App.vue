efa84eebd57b4c9450a16b721e538c4faa31733e
Rontanamobay <109564966+Rontanamobay@users.noreply.github.com>
2023-11-01 12:01:12 -0400

    deployment subcommit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -12,12 +12,12 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css?family=Song Myung:700|Noto Sans Nandinagari:400');
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,300;0,6..12,400;0,6..12,700;0,6..12,800;1,6..12,300;1,6..12,400;1,6..12,700;1,6..12,800&display=swap');
 
-.font-body {
-  font-family: 'Noto Sans Nandinagari', sans-serif;
-}
 .font-heading {
-  font-family: 'Song Myung', sans-serif;
+  font-family: 'Nunito Sans', sans-serif;
+}
+.font-body {
+  font-family: 'Nunito Sans', sans-serif;
 }
 </style>
