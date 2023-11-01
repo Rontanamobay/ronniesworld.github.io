9f0de89586fd627cdbe0bd755654d4237bdd735c
Rontanamobay <109564966+Rontanamobay@users.noreply.github.com>
2023-11-01 14:25:55 -0400

    precommit1

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,20 +4,22 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div name="wrapper" class=" w-screen h-screen bg-background-50">
-    <h1 class=" font-heading text-4xl text-text-950 font-bold">hello gau</h1>
-    <RouterView />  
+  <div name="wrapper" class="fixed w-screen h-screen bg-background-50">
+    <header name="header" class="font-body text-base flex fixed w-screen border-b border-b-text-900">
+      <h1 class="my-4 ml-8 text-text-950">ronnies-world.net</h1>
+      <nav class="ml-auto mr-8 my-auto">
+        <ul class=" list-none flex">
+          <li class="pr-6">Home</li>
+          <li class="pr-6">Blog</li>
+          <li class="">Contact</li>
+        </ul>
+      </nav>
+    </header>
+    <RouterView class=" mt-20" />  
   </div>
   
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,300;0,6..12,400;0,6..12,700;0,6..12,800;1,6..12,300;1,6..12,400;1,6..12,700;1,6..12,800&display=swap');
 
-.font-heading {
-  font-family: 'Nunito Sans', sans-serif;
-}
-.font-body {
-  font-family: 'Nunito Sans', sans-serif;
-}
 </style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 
 </script>
 
 <template>
-  <main>
-    
-  </main>
+  <div name="start wrapper" class=" font-heading text-center px-8">
+    <h1 class="font-bold text-3xl text-primary-700 mt-64 mb-6">Welcome! My Name is Ronald Gaines!</h1>
+    <h2 class=" text-secondary-300 text-xl">Uhoh! It looks like my home page is still under construction! Come back soon to find out what I am doing!
+      Meanwhile, check out my
+    </h2>
+
+  </div>
 </template>
