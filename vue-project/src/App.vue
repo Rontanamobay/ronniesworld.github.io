acd7214b72d4d24555b6317b33637184ec7f4a5f
Rontanamobay <109564966+Rontanamobay@users.noreply.github.com>
2023-11-01 11:20:43 -0400

    Initialized vue project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div name="wrapper" class=" w-screen h-screen bg-background-50">
+    <h1 class=" font-heading text-4xl text-text-950 font-bold">hello gau</h1>
+    <RouterView />  
+  </div>
+  
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Song Myung:700|Noto Sans Nandinagari:400');
+
+.font-body {
+  font-family: 'Noto Sans Nandinagari', sans-serif;
+}
+.font-heading {
+  font-family: 'Song Myung', sans-serif;
+}
+</style>
